2de3479670d95b9dade92f68a02532c0194d06bd
Ushan <ushanmalintha123@gmail.com>
2026-01-17 20:04:30 +0530

    Second update

--- data-structures/Implementable-ds/Arrays/TypesOfArrays.java
+++ data-structures/Implementable-ds/Arrays/TypesOfArrays.java
@@ -1,16 +1,32 @@
 public class TypesOfArrays {
     public static void main(String[] args) {
-        // fixed length Array
-        String names[] = {"Adam", "Kevin", "Nuwan"};
+        // 1. Fixed-length (single-dimensional) array
+        String[] names = {"Adam", "Kevin", "Nuwan"};
 
-        // Multi-Dimensional Array
-        int numbers[] = {
+        // 2. Multi-dimensional array (2D array)
+        int[][] numbers = {
             {10, 20, 30, 40, 50},
             {5, 10, 15, 20, 25},
             {2, 4, 6, 8, 10}
         };
 
-        
+        // 3. Jagged array (array of arrays with different lengths)
+        int[][] jagged = {
+            {1, 2, 3},
+            {4, 5},
+            {6, 7, 8, 9}
+        };
 
+        // 4. Dynamic array using ArrayList (resizable)
+        java.util.ArrayList<String> dynamicNames = new java.util.ArrayList<>();
+        dynamicNames.add("Alice");
+        dynamicNames.add("Bob");
+        dynamicNames.add("Charlie");
+
+        // Print examples
+        System.out.println("Fixed array: " + java.util.Arrays.toString(names));
+        System.out.println("2D array element [1][2]: " + numbers[1][2]); // 15
+        System.out.println("Jagged array element [2][3]: " + jagged[2][3]); // 9
+        System.out.println("Dynamic array: " + dynamicNames);
     }
 }
